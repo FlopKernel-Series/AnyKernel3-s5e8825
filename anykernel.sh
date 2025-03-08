a51eeca4ce8fdb644ec8fced1755a6169124e268
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-03-08 13:50:16 -0300

    Floppy v4.0.2
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FloppyKernel v4.0.1 for Exynos 1280 (unified) by @Flopster101
+kernel.string=FloppyKernel v4.0.2 for Exynos 1280 devices by @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=0
