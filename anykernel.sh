af8ab747c7239acf7dd1adc3fd8e453ab2f78617
Flopster101 <nahuelgomez329@gmail.com>
2026-02-01 14:56:59 -0300

    FLOPPY: v6.2.2
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FloppyKernel v6.2.1 for Exynos 1280 devices by @Flopster101
+kernel.string=FloppyKernel v6.2.2 for Exynos 1280 devices by @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=0
